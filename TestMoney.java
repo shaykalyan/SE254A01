18075f2700d95e2fcb3919be67a992d2182c3487
Akshay Kalyan <akshaykalyan@outlook.com>
2013-07-25 12:10:54 +1200

    Added Test file

---
+++ TestMoney.java
@@ -0,0 +1,21 @@
+package se254.money;
+/**
+ * SOFTENG 254 2011 Assignment 1 submission
+ *
+ * Author: (name, upi)
+ **/
+
+import junit.framework.TestCase;
+
+public class TestMoney extends TestCase {
+
+    // Your tests here.
+
+    /**
+     * DO NOT DELETE THIS
+     * This is needed for the automatic marking process.
+     **/
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(TestInheritanceModel.class);
+    }
+}
